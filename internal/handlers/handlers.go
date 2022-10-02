34b0bb6753ec65d80f0bc76520a9c685dbaa59c5
djoudi-cherfi <djoudi.cherfi@gmail.com>
2022-10-02 19:17:54 +0200

    Ajout de la base layout pour les fichiers templates, mise à jour dé la fonction renderTemplate et création de la fonction createTemplateCache dans le fichier handlers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"bytes"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -17,14 +19,53 @@ func Contact(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "contact")
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	t, err := template.ParseFiles("./ui/html/templates/" + tmpl + ".page.tmpl")
+func renderTemplate(w http.ResponseWriter, tmplName string) {
+	templateCache, err := createTemplateCache()
 
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		panic(err)
+	}
 
+	// templateCache["home.page.tmpl"]
+	tmpl, ok := templateCache[tmplName+".page.tmpl"]
+
+	if !ok {
+		http.Error(w, "Le template n'existe pas !", http.StatusInternalServerError)
 		return
 	}
 
-	t.Execute(w, nil)
+	buffer := new(bytes.Buffer)
+	tmpl.Execute(buffer, nil)
+	buffer.WriteTo(w)
+}
+
+func createTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/templates/*.page.tmpl")
+
+	if err != nil {
+		return cache, err
+	}
+
+	// fmt.Println(pages)
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		tmpl := template.Must(template.ParseFiles(page))
+
+		layouts, err := filepath.Glob("./ui/html/layouts/*.layout.tmpl")
+
+		if err != nil {
+			return cache, err
+		}
+
+		if len(layouts) > 0 {
+			tmpl.ParseGlob("./ui/html/layouts/*.layout.tmpl")
+		}
+
+		cache[name] = tmpl
+	}
+
+	return cache, nil
 }
